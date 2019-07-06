f54cbfc444954c3bf2bf07cdf64b5b1af3fc9809
root <root@deus-ex-machine.protei>
2019-07-06 16:39:48 +0300

    aa

---
+++ ninth_homework/test_run.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3.6
+# -*- coding: UTF-8 -*-
+
+
+import allure
+import logging
+
+
+@allure.title("001 Critical: Looking for eth0 status")
+@allure.severity("critical")
+def test001():
