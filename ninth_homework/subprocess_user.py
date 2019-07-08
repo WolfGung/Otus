92f3adc3202e8be76d325f70c8c3c87740dc1f0f
root <root@deus-ex-machine.protei>
2019-07-08 11:54:49 +0300

    ninth homework completed

---
+++ ninth_homework/subprocess_user.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python3.6
+# -*- coding: UTF-8 -*-
+
+
+import logging
+import subprocess
+
+
+class SubprocessUser:
+    def __init__(self):
+        self.host = '127.0.0.1'
+        self.output = None
+        self.command = list()
+        self.data = None
+        self.interface_state = None
+        self.model = list()
+
+    def check_output(self, arg):
+        for i in arg:
+            self.command.append(i)
+        try:
+            self.output = subprocess.check_output(arg, shell=True)
+            if self.output is not None:
+                logging.debug("System output is {}".format(self.output))
+            else:
+                logging.info("There are no output for command {}".format(self.command))
+        finally:
+            return self.output
+
+    def byte_parser(self, byte_code):
+        data = (byte_code.decode(encoding="ISO-8859-1"))
+        self.data = data.split()
+        logging.info("Converted byte_code looks like: {}".format(self.data))
+        return self.data
+
+    def interface_state_parser(self):
+        counter = 0
+        for i in self.data:
+            counter += 1
+            if i == 'state':
+                self.interface_state = self.data[counter]
+        if self.interface_state is not None:
+            logging.info("Interface state is: {}".format(self.interface_state))
+        else:
+            logging.error("Can't parse interface state!")
+        return self.interface_state
+
+    def interface_status_checker(self):
+        if self.interface_state == "UP":
+            logging.info("Status successfully checked")
+        else:
+            raise AttributeError("Interface is not UP!")
+
+    def default_route_checker(self, default_method, default_ip):
+        if default_method and default_ip in self.data:
+            logging.info("Default route ip is: {} and protocol: {}".format(default_ip, default_method))
+        else:
+            raise AttributeError("Default route doesn't corresponds to inputed")
+
+    def cpu_info_model_parser(self):
+        counter = 0
+        for i in self.data:
+            counter += 1
+            if i == "model" and self.data[counter] == "name":
+                for j in range(4):
+                    counter += 1
+                    self.model.append(self.data[counter])
+        self.model.remove(':')
+        return self.model
+
+    def cpu_info_checker(self, *cpu_parameters):
+        for par in cpu_parameters:
+            if par not in self.model:
+                raise AttributeError("Parameter {} not in cpu information".format(par))
+            else:
+                continue
+
+    def temperature_checker(self):
+        for i in self.data:
+            temp = int(i[1:3])
+            if temp > 80:
+                raise ArithmeticError("Temperature of cpu more than 80 degrees!!!")
+            else:
+                continue
+
+    @staticmethod
+    def process_coast_checker(parsed_coast):
+        for i in parsed_coast:
+            if float(i) > float(50):
+                raise ArithmeticError("Cpu coast of process is much then 50%!!!")
+            else:
+                continue
+
+    def service_status_checker(self):
+        if self.data[1] != 'active':
+            raise Warning("Service is not active!")
+            logging.error("Service status is {}!".format(self.data[1]))
+        else:
+            logging.info("Service is active...")
--- ninth_homework/test_run.py
+++ ninth_homework/test_run.py
@@ -2,10 +2,203 @@
 # -*- coding: UTF-8 -*-
 
 
+import pytest
 import allure
 import logging
+from ninth_homework.subprocess_user import SubprocessUser
 
 
 @allure.title("001 Critical: Looking for eth0 status")
 @allure.severity("critical")
+@pytest.mark.test001
 def test001():
+    with allure.step("Input command and take output"):
+        process = SubprocessUser()
+        command = "ip a | grep enp1s0"
+        output = process.check_output(command)
+    with allure.step("Parse main interface state"):
+        process.byte_parser(output)
+        process.interface_state_parser()
+    with allure.step("Checking that main interface is UP"):
+        process.interface_status_checker()
+
+
+@allure.title("002 Critical: Looking for default route")
+@allure.severity("critical")
+@pytest.mark.test002
+def test002():
+    with allure.step("Input command and take output"):
+        process = SubprocessUser()
+        command = "ip r | grep default"
+        output = process.check_output(command)
+    with allure.step("Parse default route data"):
+        process.byte_parser(output)
+    with allure.step("Checking default route state"):
+        process.default_route_checker(default_ip='192.168.0.254', default_method='dhcp')
+
+
+@allure.title("003 Critical: Looking for cpu workload")
+@allure.severity("critical")
+@pytest.mark.test003
+def test003():
+    with allure.step("Input command and take output"):
+        process = SubprocessUser()
+        command = "sensors | grep Core | awk '{print $3}'"
+        output = process.check_output(command)
+    with allure.step("Parse cpu temperature"):
+        process.byte_parser(output)
+    with allure.step("Checking temperature"):
+        process.temperature_checker()
+
+
+@allure.title("004 Critical: Looking for cpu info")
+@allure.severity("critical")
+@pytest.mark.test004
+def test004():
+    with allure.step("Input command and take output"):
+        process = SubprocessUser()
+        command = "cat /proc/cpuinfo"
+        output = process.check_output(command)
+    with allure.step("Parse cpu information"):
+        process.byte_parser(output)
+        process.cpu_info_model_parser()
+    with allure.step("Checking cpu information"):
+        cpu_parameters = ['Intel(R)', 'Core(TM)', 'i3-7100']
+        process.cpu_info_checker(*cpu_parameters)
+
+
+@allure.title("005 Critical: Looking for process resource costs")
+@allure.severity("critical")
+@pytest.mark.test005
+def test005():
+    with allure.step("Input command and take output"):
+        process = SubprocessUser()
+        command = "ps aux | awk '{print $3}'"
+        output = process.check_output(command)
+    with allure.step("Parse process information"):
+        coast = process.byte_parser(output)
+        parsed_coast = coast[1:]
+    with allure.step("Checking resource costs"):
+        process.process_coast_checker(parsed_coast)
+
+
+@allure.title("006 Critical: Looking for web interfaces statistic")
+@allure.severity("critical")
+@pytest.mark.test006
+def test006():
+    with allure.step("Input command and take output"):
+        process = SubprocessUser()
+        command = "ifconfig"
+        output = process.check_output(command)
+    with allure.step("Parse ifconfig statistic"):
+        ifconfig = process.byte_parser(output)
+    with allure.step("Print and logging statistic"):
+        print(ifconfig)
+        logging.info("Interface statistic: {}".format(ifconfig))
+
+
+@allure.title("007 Critical: Looking for service status")
+@allure.severity("critical")
+@pytest.mark.test007
+def test007():
+    with allure.step("Input command and take output"):
+        service_name = "apache2"
+        process = SubprocessUser()
+        command = "service {} status | grep Active".format(service_name)
+        output = process.check_output(command)
+    with allure.step("Parse ifconfig statistic"):
+        process.byte_parser(output)
+    with allure.step("Checking service status"):
+        process.service_status_checker()
+
+
+@allure.title("008 Critical: Looking for tcp/udp port status")
+@allure.severity("critical")
+@pytest.mark.test008
+def test008():
+    with allure.step("Input command and take output"):
+        process = SubprocessUser()
+        command = "netstat -ntlp | grep LISTEN | awk '{print $4}'"
+        output = process.check_output(command)
+    with allure.step("Parse port statistic"):
+        port_statistic = process.byte_parser(output)
+    with allure.step("Printing and logging udp/tcp ports with LISTEN status"):
+        print(port_statistic)
+        logging.info("UDP/TCP ports with LISTEN status: {}".format(port_statistic))
+
+
+@allure.title("009 Critical: Looking for package version")
+@allure.severity("critical")
+@pytest.mark.test009
+def test009():
+    with allure.step("Input command and take output"):
+        package_name = "php-curl"
+        process = SubprocessUser()
+        command = "dpkg --list | grep {} | awk '{{print $3}}'".format(package_name)
+        output = process.check_output(command)
+    with allure.step("Parse package version"):
+        package_version = process.byte_parser(output)
+    with allure.step("Printing and logging package version"):
+        print(package_version)
+        logging.info("Package {} has version {}".format(package_name, package_version))
+
+
+@allure.title("010 Critical: Looking for files in directory")
+@allure.severity("critical")
+@pytest.mark.test010
+def test010():
+    with allure.step("Input command and take output"):
+        directory = "/home/zhukov/Documents"
+        command = "cd {}; ls -l".format(directory)
+        process = SubprocessUser()
+        output = process.check_output(command)
+    with allure.step("Parse directory content"):
+        content = process.byte_parser(output)
+    with allure.step("Printing and logging directory content"):
+        print(content)
+        logging.info("Directory with path '{}' has content '{}'".format(directory, content))
+
+
+@allure.title("011 Critical: Looking for current directory")
+@allure.severity("critical")
+@pytest.mark.test011
+def test011():
+    with allure.step("Input command and take output"):
+        command = "echo $PWD"
+        process = SubprocessUser()
+        output = process.check_output(command)
+    with allure.step("Parse current directory information"):
+        dir_info = process.byte_parser(output)
+    with allure.step("Printing and logging current directory path"):
+        print(dir_info)
+        logging.info("Current directory path: {}".format(dir_info))
+
+
+@allure.title("012 Critical: Looking for core version")
+@allure.severity("critical")
+@pytest.mark.test012
+def test012():
+    with allure.step("Input command and take output"):
+        command = "uname -a"
+        process = SubprocessUser()
+        output = process.check_output(command)
+    with allure.step("Parse core information"):
+        core_info = process.byte_parser(output)[2]
+    with allure.step("Printing and logging core info"):
+        print(core_info)
+        logging.info("Core version is: {}".format(core_info))
+
+
+@allure.title("013 Critical: Looking for operation system version")
+@allure.severity("critical")
+@pytest.mark.test013
+def test013():
+    with allure.step("Input command and take output"):
+        command = " lsb_release -a | grep Description | awk '{print $2 $3 $4}'"
+        process = SubprocessUser()
+        output = process.check_output(command)
+    with allure.step("Parse operation system information"):
+        os_info = process.byte_parser(output)
+    with allure.step("Printing and logging operation system info"):
+        print(os_info)
+        logging.info("Core version is: {}".format(os_info))
